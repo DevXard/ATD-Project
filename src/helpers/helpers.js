c297aac10bafc2ac1ede9cbc4ac5285c3fee18d8
Pavlin Penev <pavlinpenev@Pavlins-MBP.fios-router.home>
2021-06-04 16:47:54 -0400

    document helper function

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,20 +1,23 @@
 const getIngredients = (data) => {
+    // get all keys that have "StrIngreadiet in there name"
     const ing = Object.keys(data).filter( key => key.indexOf('strIngredient') === 0)
         .reduce((newData, key) => {
         newData[key] = data[key]
         return newData
         }, {})
- 
+    // get all keys that have strMeasure in there name
     const meas = Object.keys(data).filter( key => key.indexOf('strMeasure') === 0)
         .reduce((newData, key) => {
         newData[key] = data[key]
         return newData
         }, {})
  
- 
+    //convert the values of the object int an array
     const ingList = Object.values(ing)
     const measList = Object.values(meas)
  
+    //transform the 2 arrays int one array with there values in to a string
+    //and return an array
     const IngreadientAndMeasure = ingList.map((val, idx) => {
         if(val){
             return `${val}: ${measList[idx]}`
